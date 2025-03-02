419d7277435e63d5d1f65e75536aab2c6c047b48
Mateus Neri <mateuslimaneri@gmail.com>
2025-03-02 01:02:06 -0300

    #02 - Ctrl+s save command

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,4 +3,5 @@ vim.keymap.set("n", "<Tab>", ">>", opts)
 vim.keymap.set("n", "<S-Tab>", "<<", opts)
 vim.keymap.set("v", "<Tab>", ">gv", opts)
 vim.keymap.set("v", "<S-Tab>", "<gv", opts)
+vim.keymap.set("n", "<C-s>", ":wa<CR>", opts)
 
